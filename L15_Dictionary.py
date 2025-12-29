2740bcb2848040d2b1b7b3241c783385fa42b36c
Anurag Sharma <mranuragsharma01@gmail.com>
2025-12-29 18:49:04 +0530

    commit

--- L15_Dictionary.py
+++ L15_Dictionary.py
@@ -17,15 +17,24 @@ print(a.items())   #retrun as view object
 print(a.get("b"))  #if dont have that key it will return none
 a["new"]="anurag sharma" #adding key value pair
 a["b"]="B" #modify
+a.update({"b":"aryan"})
 del a[2]
 a.pop("b")
 
+
 #looping
+#print all the key
 for key in a:
     print(key)
+#print all the values    
 for value in a:
     print(a[value])
+#print all the values
 for value in a.values():
     print(value)
+#print all the keys
+for x in a.keys():
+  print(x)   
+#print all the keys and values 
 for key, value in a.items():
     print(key,value)
