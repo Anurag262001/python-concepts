8b4cc26a1464c11e714f52e8af385a789c6ef1d8
Anurag Sharma <mranuragsharma01@gmail.com>
2026-01-27 10:25:18 +0530

    27-01-2026

--- L27_filehandling.py
+++ L27_filehandling.py
@@ -1,3 +1,5 @@
+""" writing the file  """
+
 f=open("f1.txt","w")
 f.write("hello this is my first file")
 f.close()
@@ -6,3 +8,15 @@ f=open("f2.txt","w")
 l=[45,2,3,4,5,6,7,88,9,9,5,43,3,5,56,7]
 for num in l:
     f.write(str(num))
+
+
+f=open("f2.txt","a")
+l=[45,2,3,4,5,6,7,88,9,9,5,43,3,5,56,7]
+for num in l:
+    f.write(str(num))
+
+
+""" Reading the file """
+f=open("f2.txt","r")
+print(f.readline())
+f.close()
